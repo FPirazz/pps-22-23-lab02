9df78cb30ceb62f611cdb45556b833b16c3f8f8d
FPirazz <federico.pirazzoli@studio.unibo.it>
2023-03-18 19:10:05 +0100

    Done Exercise 6

--- src/main/scala/u02/Esercizio2a.scala
+++ src/main/scala/u02/Esercizio2a.scala
@@ -92,3 +92,36 @@ object Esercizio2a extends App:
 
   def genCompose[X](f: X => X, g: X => X): X => X = x => f(g(x))
   println(genCompose[Int](_ - 1, _ * 2)(5)) // 9
+  println()
+
+  //Exercise 6
+
+  def time[R](block: => R): R = {
+    val t0 = System.nanoTime()
+    val result = block // call-by-name
+    val t1 = System.nanoTime()
+    println("Elapsed time: " + (t1 - t0) + "ns")
+    result
+  }
+  def gcd(a: Int, b: Int): Int = b != 0 && a > b match
+    case true => gcd(b, a % b)
+    case false => a
+
+  println(gcd(12, 8)) //4
+  println(gcd(14, 7)) //7
+  println()
+
+  time{ gcd(12, 8) }
+
+
+
+  @annotation.tailrec
+  def gcdTail(a: Int, b: Int): Int = b != 0 && a > b match
+    case false => a
+    case true => gcdTail(b, a % b)
+
+  println(gcdTail(12, 8)) //4
+  println(gcdTail(14, 7)) //7
+  println()
+
+  time{ gcdTail(12, 8) }
