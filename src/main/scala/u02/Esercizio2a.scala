65006e0ec42a0563a46fba6540f5763b3e6f2505
FPirazz <federico.pirazzoli@studio.unibo.it>
2023-03-18 21:37:32 +0100

    Done Exercise 8

--- src/main/scala/u02/Esercizio2a.scala
+++ src/main/scala/u02/Esercizio2a.scala
@@ -147,3 +147,51 @@ object Esercizio2a extends App:
   println(contains(Shape.Square(4, (0, 0)), (3, 2))) //true
   println(contains(Shape.Square(4, (0, 0)), (3, 5))) //false
   println()
+
+
+  //Exercise 8
+  enum Option[A]:
+    case Some(a: A)
+    case None() // here parens are needed because of genericity
+
+  object Option :
+    def isEmpty[A](opt: Option[A]): Boolean = opt match
+      case None() => true
+      case _ => false
+
+    def orElse[A, B >: A](opt: Option[A], orElse: B): B = opt match
+      case Some(a) => a
+      case _ => orElse
+
+    def flatMap[A, B](opt: Option[A]) (f: A => Option [B]): Option [B] = opt match
+      case Some(a) => f(a)
+      case _ => None()
+
+    def filter[A](opt: Option[A]) (f: A => Boolean): Option[A] = opt match
+      case Some(a) => if f(a) then Some(a) else None()
+      case None() => None()
+
+    def map[A](opt: Option[A])(f: A => Boolean): Option[Boolean] = opt match
+      case Some(a) => if f(a) then Some(true) else Some(false)
+      case None() => None()
+
+    def fold[A](opt: Option[A])(value: A)(f: A => A): A = opt match
+      case Some(a) => f(a)
+      case None() => value
+
+
+  import Option .*
+
+  println(filter(Some(5))(_ > 2)) //Some(5)
+  println(filter(Some(5))(_ > 8)) //None
+  println(filter(None[Int]())(_ > 2)) //None
+  println()
+
+  println(map(Some(5))(_ > 2)) //Some(true)
+  println(map(Some(5))(_ > 8)) //Some(false
+  println(map(None[Int]())(_ > 2)) //None
+  println()
+
+  println(fold(Some(5))(1)(_ + 1)) // 6
+  println(fold(None[Int]())(1)(_ + 1)) // 1
+  println()
