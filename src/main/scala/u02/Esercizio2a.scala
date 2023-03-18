d8773d3aa5ff60ff33c2d2b229a40a8aacbe1124
FPirazz <federico.pirazzoli@studio.unibo.it>
2023-03-18 17:54:26 +0100

    Done exercise 4

--- src/main/scala/u02/Esercizio2a.scala
+++ src/main/scala/u02/Esercizio2a.scala
@@ -1,7 +1,8 @@
 package u02
 
 object Esercizio2a extends App:
-  //Exercise a
+  //Exercise 3
+  //Point a
 
   //Lambda
   def positiveFun(x: Int): String = x match
@@ -24,7 +25,7 @@ object Esercizio2a extends App:
   println()
 
 
-  //Exercise b
+  //Point b
   val empty: (String) => (Boolean) = _ == ""
   val negLambda: (String => Boolean) => String => Boolean =
     f => s => !f(s)
@@ -50,7 +51,7 @@ object Esercizio2a extends App:
   println()
 
 
-  //Exercise c
+  //Point c
   def negGen[X](f: (X => Boolean)): (X => Boolean) =
     s => !f(s)
 
@@ -63,13 +64,27 @@ object Esercizio2a extends App:
   println()
 
   //Exercise2b
+  //Point 4
   val p1: Int => Int => Int => Boolean = x => y => z => x <= y && x <= z && y == z
-
   println(p1(1)(2)(2))
+  println(p1(1)(2)(3))
+  println()
 
-  //val p2:
-  //def p3()
-  //def p4()
+  val p2: (Int, Int, Int) => Boolean = (x, y, z) => x <= y && x <= z && y == z
+  println(p2(1, 2, 2)) //true
+  println(p2(1, 2, 3)) //false
+  println()
+
+  def p3(x: Int)(y: Int)(z: Int): Boolean = x <= y && x <= z && y == z
+  val p3test = p3(1)(2)
+  println(p3test(2)) //true
+  println(p3test(3)) //false
+  println()
+
+  def p4(x: Int, y: Int, z: Int): Boolean = x <= y && x <= z && y == z
+  println(p4(1, 2, 2)) //true
+  println(p4(1, 2, 3)) //false
+  println()
 
 
 
